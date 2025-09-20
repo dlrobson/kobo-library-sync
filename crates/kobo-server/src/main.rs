74bf4a0c4a230b7cf2b981d93ced18cabdeabf5a
Daniel Robson <danr.236@gmail.com>
2025-09-20 18:57:42 +0000

    chore: adds a basic interceptor for requests

--- crates/kobo-server/src/main.rs
+++ crates/kobo-server/src/main.rs
@@ -4,10 +4,24 @@ use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/path", get(|| async { "Hello, World!" }))
+        .layer(axum::middleware::from_fn(print_request_response));
+
+    tracing::info!("Listening on http://0.0.0.0:3000");
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn print_request_response(
+    req: axum::extract::Request,
+    next: axum::middleware::Next,
+) -> Result<impl axum::response::IntoResponse, (axum::http::StatusCode, String)> {
+    tracing::debug!("Incoming request: {} {}", req.method(), req.uri());
+
+    Ok(next.run(req).await)
 }
